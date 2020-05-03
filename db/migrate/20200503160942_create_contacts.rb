769744cc48d57fa4d349f63ac3db125189fa9b2c
MarceloRC <marcelorc2013@gmail.com>
2020-05-03 11:11:33 -0700

    feat: create contact

---
+++ db/migrate/20200503160942_create_contacts.rb
@@ -0,0 +1,10 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
