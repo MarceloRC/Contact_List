0c697ffcd0086f51083333cd85162f39d7df94f6
MarceloRC <marcelorc2013@gmail.com>
2020-05-04 20:31:42 -0700

    feat: update sesseions user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :contacts
   root to: "static_pages#index"
 
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  delete 'sair', to: 'sessions#destroy'
 
-  resources :users, olny: [:new, :create]
+  resources :contacts
+  resources :users, only: [:new, :create, :show]
 end
